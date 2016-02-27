c138372ba9b41a8a2d9fdfc730bf4dc637c9d021
Juzer Nomani <jnomani@vt.edu>
2016-02-27 03:58:33 -0500

    Get login to work

---
+++ Cartera/app/src/main/java/cartera/icaninter/net/cartera/Cartera.java
@@ -0,0 +1,29 @@
+package cartera.icaninter.net.cartera;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+import com.parse.interceptors.ParseLogInterceptor;
+
+/**
+ * Created by Juzer on 2/27/2016.
+ */
+public class Cartera extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("master")
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server("http://cartera-server.herokuapp.com/parse/")
+                .build());
+
+
+    }
+
+
+
+}
